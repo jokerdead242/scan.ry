7c58164e676aa3b6041bd99296d0a1b9d7d023bf
jokerdead242 <32109105+jokerdead242@users.noreply.github.com>
2025-08-08 08:51:38 +0000

    243

--- scan.py
+++ scan.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import numpy as np
 
 # === Настройки ===
-INTERVAL = "1m"
+INTERVAL = "3m"
 LIMIT = 200
 SLEEP_TIME = 60  # 1 минута
 BINANCE_FUTURES_ENDPOINT = "https://fapi.binance.com"
